d53e430dbbd18ab55105a595d1f01318b53833b1
Salman Ahmed <salmanahmedx@hotmail.com>
2022-05-12 11:35:00 +0600

    checkbox animation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const containers = document.querySelectorAll(".input-container");
 const form = document.querySelector("form");
 
+//timeline
 const tl = gsap.timeline({ default: { duration: 1 } });
 
 //line
@@ -89,3 +90,31 @@ function modifyColor(color, line, placeholder) {
     gsap.to(line, { stroke: color, duration: 0.75 })
     gsap.to(placeholder, { color: color, duration: 0.75 })
 }
+
+//Checkbox animation fill
+
+const checkbox = document.querySelector(".checkbox");
+const tl2 = gsap.timeline({ defaults: { duration: 0.5, ease: "Power2.easeOut" } })
+
+const tickMarkPath = document.querySelector(".tick-mark path")
+const pathLength = tickMarkPath.getTotalLength();
+
+gsap.set(tickMarkPath, {
+    strokeDashoffset: pathLength,
+    strokeDasharray: pathLength
+})
+
+checkbox.addEventListener("click", () => {
+    if (checkbox.checked) {
+        tl2.to(".checkbox-fill", { top: "0%" });
+
+        tl2.fromTo(tickMarkPath, { strokeDashoffset: pathLength }, { strokeDashoffset: 0 }, "<50%");
+
+        tl2.to(".checkbox-label", { color: "#6391e8" }, "<");
+
+    } else {
+        tl2.to(".checkbox-fill", { top: "100%" });
+        tl2.fromTo(tickMarkPath, { strokeDashoffset: 0 }, { strokeDashoffset: pathLength });
+        tl2.to(".checkbox-label", { color: "#c5c5c5" }, "<10%");
+    }
+})
